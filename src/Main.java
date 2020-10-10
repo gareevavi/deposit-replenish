cb01cef61c34a1e5d7781a4ac9522ae80e1bfca8
vigareeva <nera888@list.ru>
2020-10-10 10:36:04 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int deposit = 1000;
+        int percent = 3;
+        System.out.println(deposit * percent / 100);
+
+    }
+
+
+}
